939bbb211b47eb303098ed571754ffb983f9b5c1
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-05-31 01:37:11 -0500

    add: add components sidebar and add pages home

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
+//Components
+import SideBard from "./components/sidebar/SideBard";
 import TopBar from "./components/topbar/TopBar";
+
+//styles
+import "./app.css";
 
 function App() {
   return (
     <div>
       <TopBar />
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
-      test<br/>
+      <div className="container">
+        <SideBard />
+        <div className="others">
+          other pages
+        </div>
+      </div>
     </div>
   );
 }
