0f875a3ec727ca62d0256077a2e96a7691cc5d3f
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-06-01 00:39:19 -0500

    add: add component chart and add librarie recharts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 //Components
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo"
-
+import Chart from '../../components/chart/Chart'
 
 //Styles
 import "./home.css"
+
+//Data
+import {userData} from "../../dummyData"
 
 export default function Home() {
     return (
         <div className='home'>
             <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
         </div>
     )
 }
