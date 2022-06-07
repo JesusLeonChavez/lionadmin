856fdd3aa129cc65dd0ac1f9fc8f6d34dba2d15c
LEONIDAS <jesus.leon1@unmsm.edu.pe>
2022-06-07 17:52:41 -0500

    feat: add component productList

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
 
 //styles
 import "./app.css";
@@ -25,6 +26,9 @@ function App() {
           <Route path="/users" element={<UserList />} />
           <Route path="/user/:userId" element={<User />} />
           <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<User />} />
+          <Route path="/newproduct" element={<NewUser />} />
         </Routes>
       </div>
     </Router>
